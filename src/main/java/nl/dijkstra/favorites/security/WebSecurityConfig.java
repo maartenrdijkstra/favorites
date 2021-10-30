98906584ad64e8a921dcf853b0d699a6b3bb0252
Maarten Dijkstra <maartenrdijkstra@gmail.com>
2021-10-30 17:18:45 +0200

    Tried one last thing to make some REST Calls Succeed on Heroku

--- src/main/java/nl/dijkstra/favorites/security/WebSecurityConfig.java
+++ src/main/java/nl/dijkstra/favorites/security/WebSecurityConfig.java
@@ -2,17 +2,30 @@ package nl.dijkstra.favorites.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public String crossOriginAllowedHeaders = "*";
+    public String crossOriginAllowedSites = "*";
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -57,6 +70,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutUrl("/appLogout")
                 .logoutSuccessUrl("/")
-                .permitAll();
+                .permitAll()
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin().addHeaderWriter((request, response) -> {
+            response.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, private");
+            response.setHeader("Pragma", "no-cache");
+            response.setHeader("Access-Control-Allow-Origin", this.crossOriginAllowedSites);
+        });
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    protected CorsFilter crossFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.addAllowedMethod(HttpMethod.GET);
+        config.addAllowedMethod(HttpMethod.POST);
+        config.addExposedHeader("Authorization");
+        config.setMaxAge(Long.valueOf(2500));
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
